ab0d6b02cfea017e9519c7a24a2a119e4556b39c
oumouma <garveyshah1595@gmail.com>
2024-07-02 06:26:38 +0300

    Feat: CheckFileExist checks if a file exists at the specified 'filename'.

--- functions/utilities.go
+++ functions/utilities.go
@@ -4,8 +4,17 @@ import (
 	"bufio"
 	"os"
 )
+
+// Function to check if a file exists at the specified 'filename'.
+func CheckFileExits(filename string) bool {
+	_,  err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+
 // ReadFileLines reads all lines from a file and returns them as a slice of string.
 func ReadFileLines(filename string) ([]string, error) {
+	
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
@@ -24,3 +33,10 @@ func ReadFileLines(filename string) ([]string, error) {
 	}
 	return lines, nil
 }
+
+// // Function prints the output data in a the specific desirable format.
+// func PrintResults() {
+// 	fmt.Printf("%d --> the standard input\n",lines[i])
+// 	fmt.Printf("%d %d  --> the range for the next, input in this case for the number %d",lines[i])
+
+// }
