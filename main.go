69180dd8c78b314514d600eeef61b84603340de0
oumouma <garveyshah1595@gmail.com>
2024-07-02 06:29:10 +0300

    called func checkfileexits in main

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 
 func main() {
 	filename := "/home/oumouma/guess-it-1/data.txt"
+	exist := functions.CheckFileExits(filename)
+	if !exist {
+		fmt.Println("Error file does not exit.")
+		return
+	}
 	lines, err := functions.ReadFileLines(filename)
 	if err != nil {
 		log.Fatalf("Error reading %s: %v", filename, err)
