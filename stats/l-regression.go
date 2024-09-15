a3ad03c72c6d20cfe0967938c0904da1a5860c1a
oumouma <garveyshah1595@gmail.com>
2024-09-16 01:05:26 +0300

    feat: functions to predict the range of data

--- stats/l-regression.go
+++ stats/l-regression.go
@@ -1,5 +1,7 @@
 package stats
 
+import "math"
+
 func LinearRegression(ys []float64) (slope, intercept float64) {
 	// Number of data points(len(xs))
 	n := float64(len(ys))
@@ -22,4 +24,35 @@ func LinearRegression(ys []float64) (slope, intercept float64) {
 	intercept = (sumY - slope*sumX) / n
 	// fmt.Println("xs := ", xs)
 	return slope, intercept
+}
+
+// Function to calculate the range the next number may fall and predic the next number employing the linear regression model.
+func Prectidor(x, slope, intercept float64) (float64, float64, float64) {
+	guess := (slope * x) + intercept
+
+	residual := x - guess
+
+	return x, guess, residual
+}
+
+func CalculateRange(guessArray []float64, stdError float64) (float64, float64) {
+	var upperB, lowerB float64
+
+	for _, guess := range guessArray {
+		upperB = guess - (stdError * 2)
+		lowerB = guess + (stdError * 2)
+	}
+	return upperB, lowerB
+}
+
+// Function to calculate Variance of the residuals
+func StandardError(residuals []float64) float64 {
+	n := len(residuals)
+	var ssd, stdError float64
+
+	for _, num := range residuals {
+		ssd += num * num
+		stdError = ssd / float64(n-2)
+	}
+	return float64(math.Sqrt(stdError))
 }
