664071dbf3b9b818c98d897729c4a00e36a5a964
oumouma <garveyshah1595@gmail.com>
2024-09-16 01:07:10 +0300

    Modify : reader function

--- utils/reader.go
+++ utils/reader.go
@@ -7,30 +7,35 @@ import (
 	"strconv"
 )
 
-func Reader(fileName string) ([]float64, error) {
+func Reader(fileName string) ([]float64, []float64, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
-		return nil, fmt.Errorf("error opeing file %v\n: %v", fileName, err)
+		return nil, nil, fmt.Errorf("error opeing file %v\n: %v", fileName, err)
 	}
 	defer file.Close()
 
 	fileScan := bufio.NewScanner(file)
 	fileScan.Split(bufio.ScanLines)
-	data := []float64{}
+	var ys, xs []float64
 
 	for fileScan.Scan() {
 		line := fileScan.Text()
 		value, err := strconv.ParseFloat(line, 64)
 		if err != nil {
-			return nil, fmt.Errorf("error parsing float from line '%v': %v", line, err)
+			return nil, nil, fmt.Errorf("error parsing float from line '%v': %v", line, err)
 		}
 
-		data = append(data, value)
+		ys = append(ys, value)
 
 		// Check if any error occurred during scanning
 		if err := fileScan.Err(); err != nil {
-			return nil, fmt.Errorf("error reader file %v: %v", fileName, err)
+			return nil, nil, fmt.Errorf("error reader file %v: %v", fileName, err)
 		}
 	}
-	return data, nil
+
+	for i := 0; i < len(ys); i++ {
+		xs = append(xs, float64(i))
+	}
+
+	return xs, ys, nil
 }
