b71d454e95c89bbf53c9aa832b9e8baa0b414369
oumouma <garveyshah1595@gmail.com>
2024-09-16 22:29:08 +0300

    chore:

--- utils/output.go
+++ utils/output.go
@@ -8,14 +8,14 @@ import (
 
 func Output(xs, ys []float64) {
 	var guess, upperB, lowerB, residual, x, stdError float64
-	slope, intercept := stats.LinearRegression( ys)
+	slope, intercept := stats.LinearRegression(ys)
 	var residsArray, guessArray []float64
 
-	fmt.Printf("Slope:     %.2f\n", slope)
-	fmt.Printf("Intercept: %.2f\n\n", intercept)
+	fmt.Printf("\033[032mSlope:\033[0m     %.2f\n", slope)
+	fmt.Printf("\033[032mIntercept:\033[0m %.2f\n\n", intercept)
 
 	for i, num := range xs {
-		x, guess, residual = stats.Prectidor(num, slope, intercept)
+		x, guess, residual = stats.Prectidor(num,ys[i], slope, intercept)
 
 		residsArray = append(residsArray, residual)
 		guessArray = append(guessArray, guess)
@@ -23,11 +23,10 @@ func Output(xs, ys []float64) {
 		stdError = stats.StandardError(residsArray)
 		upperB, lowerB = stats.CalculateRange(guessArray, stdError)
 
-		fmt.Printf("y:	%.2f\n", ys[i])
-		fmt.Printf("Guess:	%.2f\n", guess)
-		fmt.Printf("\nx: %.2f\nUpperB: %.2f\nLowerB: %.2f\nResidual: %.2f\n\n", x, upperB, lowerB, residual)
+		fmt.Printf("\033[032my:\033[0m	%.2f\n", ys[i])
+		fmt.Printf("\033[032mGuess:\033[0m	%.2f\n", guess)
+		fmt.Printf("\n\033[032mx:	\033[0m %.2f\n\033[032mUpperB:\033[0m %.2f\n\033[032mLowerB:\033[0m %.2f\n\033[032mResidual:\033[0m %.2f\n\n", x, upperB, lowerB, residual)
 
 	}
 
-	
 }
