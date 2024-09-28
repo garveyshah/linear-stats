bfde59f3f271ad41be6d420065ddd82463e64b3d
oumouma <garveyshah1595@gmail.com>
2024-09-28 06:20:13 +0300

    testing: refactore testing code to the program logic

--- stats/l-regression_test.go
+++ stats/l-regression_test.go
@@ -13,44 +13,39 @@ func isAlmostEqual(a, b, tolerance float64) bool {
 }
 
 func TestLinearRegression(t *testing.T) {
-	// Files containing test data
-	testData := []string{"../test_data/data1.txt", "../test_data/data2.txt", "../test_data/data3.txt"}
-
-	for _, dataFile := range testData {
-		ys, err := common.Reader(dataFile)
+	// Expected slope and intercept values for each dataset
+	tt := []struct {
+		input     string
+		slope     float64
+		intercept float64
+		r         float64
+		err       error
+	}{
+		{"../test_data/data1.txt", -8.742857, 153.857143, -0.5330331012, nil},
+		{"../test_data/data2.txt", 17.500000, 68.500000, 0.9891584832, nil},
+		{"../test_data/data3.txt", -2.151515, 150.781818, -0.1898733212, nil},
+	}
+	for i, tc := range tt {
+		ys, err := common.Reader(tc.input)
 		if err != nil {
-			t.Fatalf("Failed to read data from %q: %v", dataFile, err)
+			t.Fatalf("Failed to read data from %q: %v", tc.input, err)
 		}
-
-		// Expected slope and intercept values for each dataset
-		tt := []struct {
-			slope     float64
-			intercept float64
-			r         float64
-			err       error
-		}{
-			{-0.999247, 552.465663, -0.9986787077, nil},
-			// {0.8, 1.2},
-			// {-0.4, 3.5},
-		}
-
 		// Linear regression calculation for the current dataset
 		r, slope, intercept, err := LinearRegression(ys)
+		if err != tc.err {
+			t.Fatalf("Test for %q Failed (case %d) - wrong error type, got= %s, want= %s", tc.input, i, err, tc.err)
+		}
 
-		for i, tc := range tt {
-			tolerance := 0.0001 // Defination of a tolerance for floating-point comparison
+		tolerance := 0.0001 // Defination of a tolerance for floating-point comparison
+		if !isAlmostEqual(r, tc.r, tolerance) {
+			t.Fatalf("Test for %q Failed  (case %d) - wrong coeffient, got= %f, want= %f", tc.input, i, r, tc.r)
+		}
+		if !isAlmostEqual(slope, tc.slope, tolerance) {
+			t.Fatalf("Test for %q Failed  (case %d) - wrong slope, got= %f, want= %f", tc.input, i, slope, tc.slope)
+		}
 
-			if !isAlmostEqual(r, tc.slope, tolerance) {
-				t.Fatalf("Test for %q")
-			}
-
-			if !isAlmostEqual(slope, tc.slope, tolerance) {
-				t.Fatalf("Test for %q Failed  (case %d) - wrong slope, got= %f, want= %f", dataFile, i, slope, tc.slope)
-			}
-
-			if intercept != tc.intercept {
-				t.Fatalf("Test for %q Failed (case %d) - wrong intercept, got= %f, want= %f", dataFile, i, intercept, tc.intercept)
-			}
+		if !isAlmostEqual(intercept, tc.intercept, tolerance) {
+			t.Fatalf("Test for %q Failed (case %d) - wrong intercept, got= %f, want= %f", tc.input, i, intercept, tc.intercept)
 		}
 	}
 }
